747c7b35c989d2dc49edee011fb51d48ad04ecd8
Rafaela Dubeux <rdg@cesar.school>
2026-09-02 09:03:46 -0300

    adiciona as rotas do projeto

--- project/urls.py
+++ project/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', include("hello.urls")),
+    path('forum/', include('forum.urls')),
+    path('', RedirectView.as_view(url='/forum/', permanent=False), name='home'),
 ]
