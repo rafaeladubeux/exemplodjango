9ee94f9faaac65ec74d98047aff1bcb42ac2e93c
Rafaela Dubeux <rdg@cesar.school>
2026-09-09 08:39:56 -0300

    adiciona página com os dados das perguntas e as respostas

--- project/urls.py
+++ project/urls.py
@@ -21,6 +21,6 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', include("hello.urls")),
-    path('forum/', include('forum.urls')),
+    path('forum/', include("forum.urls")),
     path('', RedirectView.as_view(url='/forum/', permanent=False), name='home'),
 ]
